f4a46b6e0893813b5a30b4b0f5bd4122f0c1520d
twdeveloper <twdeveloper@outlook.com>
2022-06-16 23:42:05 +0800

    v1

---
+++ WebBMI/HealthMgr/BmiCalculator.cs
@@ -0,0 +1,55 @@
+﻿using isRock.LineBot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthMgr
+{
+    public class BmiCalculator
+    {
+        public BmiCalculator()
+        {
+            this.bot = new Bot("");
+        }
+
+
+        public BmiCalculator(Bot bot)
+        {
+            this.bot = bot;
+        }
+        private isRock.LineBot.Bot bot { get; set; }
+
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public float BMI
+        {
+            get
+            {
+                return Calculate();
+            }
+        }
+
+        public delegate void TooFatWarningEventHandler(object e, float value);
+        public event TooFatWarningEventHandler TooFatWarning;
+
+        public float Calculate()
+        {
+            float result = 0;
+            if (Weight <= 0 || Height <= 0)
+                throw new Exception();
+            //將float改成int試試看
+            float height = (float)Height / 100;
+            result = Weight / (height * height);
+            //改用注入的linebot物件
+            //isRock.LineBot.Bot bot = new isRock.LineBot.Bot("");
+            //如果BMI>30
+            if (result > 30)
+            {
+                if (TooFatWarning != null) TooFatWarning.Invoke(this, result);
+                bot.SendNotify("ydiw9kxHigB4Cx7D9momQtstobzkrfvZGr31RqOQcC5", "test");
+            }
+            return result;
+        }
+
+    }
+}
---
+++ WebBMI/HealthMgrTests/BmiCalculatorTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HealthMgr;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NSubstitute;
+
+namespace HealthMgr.Tests
+{
+    [TestClass()]
+    public class BmiCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            //arrange
+            HealthMgr.BmiCalculator bmi = new HealthMgr.BmiCalculator();
+            bmi.Height = 170;
+            bmi.Weight = 70;
+            //act
+            var result = bmi.Calculate();
+            //assert
+            Assert.AreEqual("24.22", result.ToString("00.00"));
+        }
+
+        [TestMethod]
+        public void CalculateExceptionTest()
+        {
+            //arange
+            HealthMgr.BmiCalculator bmi = new HealthMgr.BmiCalculator();
+            bmi.Height = 0;
+            bmi.Weight = 0;
+            //action
+            //var result = bmi.Calculate();
+            //Assert
+            Assert.ThrowsException<Exception>(() => { bmi.Calculate(); });
+        }
+
+        [DataTestMethod]
+        [DataRow(170, 70, "24.22")]
+        [DataRow(165, 50, "18.37")]
+        public void CalculateMultiTest(int height, int weight, string expect)
+        {
+            HealthMgr.BmiCalculator bmi = new HealthMgr.BmiCalculator();
+            bmi.Height = height;
+            bmi.Weight = weight;
+
+            var result = bmi.Calculate();
+
+            Assert.AreEqual(expect, result.ToString("00.00"));
+        }
+
+        [TestMethod]
+        public void CalculateSendMessageTest()
+        {
+            var bot = NSubstitute.Substitute.For<isRock.LineBot.Bot>("");
+            //arrange
+            HealthMgr.BmiCalculator bmi = new HealthMgr.BmiCalculator(bot);
+            bmi.Height = 170;
+            bmi.Weight = 90;
+            //action
+            var result = bmi.Calculate();
+            //Assert
+            bot.Received().SendNotify("ydiw9kxHigB4Cx7D9momQtstobzkrfvZGr31RqOQcC5", "test");
+        }
+
+        [TestMethod]
+        public void CalculateNotSendMessageTest()
+        {
+            var bot = NSubstitute.Substitute.For<isRock.LineBot.Bot>("");
+            //arrange
+            HealthMgr.BmiCalculator bmi = new HealthMgr.BmiCalculator(bot);
+            bmi.Height = 170;
+            bmi.Weight = 50;
+            //action
+            var result = bmi.Calculate();
+            //Assert
+            bot.DidNotReceive().SendNotify("ydiw9kxHigB4Cx7D9momQtstobzkrfvZGr31RqOQcC5", "test");
+        }
+
+        [TestMethod]
+        public void CalculateEventRaiseTest()
+        {
+            bool isEventFired = false;
+            //arrange
+            HealthMgr.BmiCalculator bmi = new HealthMgr.BmiCalculator();
+            bmi.TooFatWarning += (e, value) => { isEventFired = true; };
+            bmi.Height = 170;
+            bmi.Weight = 90;
+            //action
+            var result = bmi.Calculate();
+            //Assert
+            Assert.IsTrue(isEventFired);
+        }
+    }
+}
